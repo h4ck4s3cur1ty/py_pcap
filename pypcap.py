c531c83a3479b864273a5cfa6a73665e53c274a7
h4ck4s3cur1ty <qjawns0117@naver.com>
2017-07-17 20:35:08 +0900

    first upload

---
+++ pypcap.py
@@ -0,0 +1,39 @@
+import socket, binascii, struct
+import time
+
+sock = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x800))
+print "Waiting.."
+pkt = sock.recv(2048)
+print "received"
+
+def hex2ip(hex_ip):
+    addr_long = int(hex_ip,16)
+    hex(addr_long)
+    hex_ip = socket.inet_ntoa(struct.pack(">L", addr_long))
+    return hex_ip
+
+while len(pkt) > 0:
+
+    print "Waiting.."
+    pkt = sock.recv(2048)
+    print "received"
+
+    if(len(pkt)) > 54:
+        ethHeader = pkt[0:14]
+        ipHeader = pkt[14:34]
+        tcpHeader = pkt[34:38]
+
+	print 'dest mac : ' + ethHeader.encode('hex')[:12]
+	print 'src mac : ' + ethHeader.encode('hex')[12:24]
+	print 'type : ' + ethHeader.encode('hex')[24:28]
+	
+	print 'dest ip : ' + hex2ip(ipHeader.encode('hex')[24:32])
+	print 'src ip : ' + hex2ip(ipHeader.encode('hex')[32:40])
+	
+	print 'length : ' + binascii.unhexlify(ipHeader.encode('hex')[4:8])
+
+	
+	#print "from:  "+hex2ip(ipdata[0])+":"+tcpdata[0]+"    to:  "+hex2ip(ipdata[1])+":"+tcpdata[1]
+	
+    else:
+        continue
